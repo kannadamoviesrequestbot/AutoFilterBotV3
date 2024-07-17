334ea62d55c2b6c7f1297d3cecb01802bf10185a
callmebaby <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2024-07-17 15:51:21 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,27 +6,27 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
-API_ID = int(environ['API_ID', ''])
-API_HASH = environ['API_HASH', '']
-BOT_TOKEN = environ['BOT_TOKEN', '']
+API_ID = int(environ['API_ID', '7041911'])
+API_HASH = environ['API_HASH', 'abab2561c71e3004a55d4ff9763d5383']
+BOT_TOKEN = environ['BOT_TOKEN', '6400207558:AAEy6y7KZZF2LYJ7hc-hL1LL27RvRAbii2U']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", "-1001956199528"))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "995086208").split())
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://autofilter:autofilter@cluster0.en2bnta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS', ''].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS', ''].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS', '995086208'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS', '-1002226519917'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '995086208').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "-1002102214357").split()]
 # MongoDB information
 DATABASE_NAME = environ['BOT_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
